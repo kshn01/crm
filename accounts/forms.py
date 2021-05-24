bb93d7b0374088084177365e8dc1aa2dc4ab5386
kishan <k99redmi@gmail.com>
2021-05-25 00:07:34 +0530

    "completed"

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,10 @@
+import django
 from django.forms import ModelForm
 from .models import Order
 
+from django.contrib.auth.forms import UserCreationForm
+from django import  forms
+from django.contrib.auth.models import User
 
 
 
@@ -13,3 +17,11 @@ class OrderForm(ModelForm):
     class Meta:
         model = Order  #whcih model to be used 
         fields = '__all__'
+
+# -- User creation form 
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email' , 'password1', 'password2']
+
+# ---
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,22 @@
 from typing import ContextManager
 from django import forms
+from django.forms.models import construct_instance
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .forms import OrderForm
+from .forms import OrderForm, CreateUserForm
 from django.forms import formsets, inlineformset_factory
 from .filters import OrderFilter
+
+
+
+
+# -- for register.html and login.html // django provides default forms
+from django.contrib.auth.forms import UserCreationForm
+
+# --- for messages
+from django.contrib import messages
+from django.contrib.auth import authenticate,login, logout
+
 
 # Create your views here.
 
@@ -12,7 +24,56 @@ from .models import *
 
 
 
+# ---- Rehistration page
 
+def registerPage(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user)
+            return redirect('login')
+
+    
+    context ={'form' : form}
+    return render(request, 'accounts/register.html',context)
+
+# -----
+
+# --------------
+
+def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request , username =username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else: 
+            messages.info(request,'Username or pasword is incorrect')
+
+    context ={}
+    return render(request, 'accounts/login.html',context)
+
+# ----------------------------------
+
+
+# -----------------------
+
+
+def logoutUser(request):
+	logout(request)
+	return redirect('login') 
+
+
+# ---------------------
 
 
 def home(request):
