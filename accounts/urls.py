d17840d8356dddaf82ce5bac36dc98a2ea5102d1
kishan <k99redmi@gmail.com>
2021-09-27 19:24:26 +0530

    final-27-09-001

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,6 +10,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home,name='home'),
+    
     path('products/', views.products,name='products'),
     # path('customer/', views.customer,name='customer'),
 
@@ -23,10 +24,15 @@ urlpatterns = [
     path('customer/<str:pk_test>/',views.customer, name='customer'),
 
     path('create_order/<str:pk>/',views.createOrder, name='create_order'),
+
     path('update_order/<str:pk>/', views.updateOrder, name = 'update_order'),
+
     path('delete_order/<str:pk>/', views.deleteOrder, name= 'delete_order'),
+
     path('login/', views.loginPage, name= 'login'),
+    
     path('register/', views.registerPage, name= 'register'),
+    
     path('logout/', views.logoutUser, name= 'logout'),
 
     path('user/', views.userPage , name = 'user'),
